7610ae79ad12054e563f51ef5387b1bf9f976542
DzKInShO2 <83569579+DzKInShO2@users.noreply.github.com>
2025-03-17 21:32:08 +0800

    Tambahkan beberapa penjelasan

--- widget.py
+++ widget.py
@@ -13,11 +13,19 @@ class MovingLabel(QLabel):
     def __init__(self, text, parent):
         super().__init__(text, parent)
 
+        # Variabel screenWidth dan screenHeight di pake untuk kalkulasi
+        # batasan posisi random yang akan di produksi
         self.screenWidth = 640
         self.screenHeight = 640
+
+        # Minimum Width di set supaya label ndak ke clip
         self.setMinimumWidth(72)
         self.setMouseTracking(True)
 
+    # Setiap kali cursor bergerak ke atas label maka fokus dari widget Window
+    # bakal berubah ke label. Jadi event mouseMoveEvent dari Window bakal
+    # berhenti ke trigger dan event mouseMoveEvent dari label yang akan ke trigger.
+    # Hal ini bisa di mamfaatkan untuk mendeteksi apakah cursor overlap dengan label.
     def mouseMoveEvent(self, e):
         x = randint(0, self.screenWidth - self.width())
         y = randint(0, self.screenHeight - self.height())
@@ -39,9 +47,12 @@ class MynWindow(QWidget):
         self.setMouseTracking(True)
         self.label = MovingLabel("x: 0 y: 0", self)
 
+    # Gambar posisi cursor pada label
     def mouseMoveEvent(self, e):
         self.label.setText(f"x: {e.x()} y: {e.y()}")
 
+    # Resize event digunakan untuk memperbarui batasan posisi random label
+    # setelah window di resize.
     def resizeEvent(self, e):
         self.label.screenWidth = e.size().width()
         self.label.screenHeight = e.size().height()
